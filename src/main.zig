10e9ef0c37adccc50288b32d844c49839c11c55d
Brendan Callender <bren.call8@gmail.com>
2026-09-06 17:26:41 -0700

    kinda work

--- src/main.zig
+++ src/main.zig
@@ -2,13 +2,14 @@
 const std   = @import("std");
 const win32 = @import("win32.zig");
 
-const DirEntry  = win32.DirEntry;
 const ArrayList = std.ArrayList;
 const Writer    = std.Io.Writer;
 const pow       = std.math.pow;
 const sort      = std.mem.sort;
 const order     = std.mem.order;
 
+
+// TODO(bcall): set to "" if stdout is not a tty
 const BLUE  = "\x1b[34m";
 const GREEN = "\x1b[32m";
 const RESET = "\x1b[0m";
@@ -34,15 +35,15 @@ pub fn main(init: std.process.Init) !void
 
     const args = try init.minimal.args.toSlice(arena);
 
-    var dir_path: []u8 = @constCast(".");  // default path
-    var one_col: bool = false;             // list entries in 1 column
-    var include_all: bool = false;         // list files/dirs starting with .
-    var long_format: bool = false;         // list entry + additional data
-    var recurse: bool = false;             // recursively list out contents
-    var sort_mod_time: bool = false;       // sort entries by last modified time
-    var sort_size: bool = false;           // sort entries by size
-    var row_wise: bool = false;            // list entries by row then col 
-    var reverse:  bool = false;            // reverse sorting order
+    var path: []u8  = @constCast("."); // default path
+    var one_col: bool     = false;  // list entries in 1 column
+    var show_hidden: bool = false;  // list files/dirs starting with .
+    var long_format: bool = false;  // list entry + additional data
+    var recurse: bool     = false;  // recursively list out contents
+    var sort_mtime: bool  = false;  // sort entries by last modified time
+    var sort_size: bool   = false;  // sort entries by size
+    var row_wise: bool    = false;  // list entries by row then col 
+    var reverse:  bool    = false;  // reverse sorting order
 
     for (args[1..]) |arg|
     {
@@ -55,34 +56,37 @@ pub fn main(init: std.process.Init) !void
         {
             for (1..arg.len) |i|
             {
-                if (arg[i] == '1') one_col = true;
-                if (arg[i] == 'a') include_all = true;
+                if (arg[i] == '1') one_col     = true;
+                if (arg[i] == 'a') show_hidden = true;
                 if (arg[i] == 'l') long_format = true;
-                if (arg[i] == 'R') recurse = true;
-                if (arg[i] == 'r') reverse = true;
-                if (arg[i] == 'S') sort_size = true;
-                if (arg[i] == 't') sort_mod_time = true;
-                if (arg[i] == 'x') row_wise = true;
+                if (arg[i] == 'R') recurse     = true;
+                if (arg[i] == 'r') reverse     = true;
+                if (arg[i] == 'S') sort_size   = true;
+                if (arg[i] == 't') sort_mtime  = true;
+                if (arg[i] == 'x') row_wise    = true;
             }
         }
         else
         {
-            // WARN(bcall): directory will be set to last argument not starting with '-'
-            dir_path = @ptrCast(@constCast(arg));
+            // NOTE(bcall): directory will be set to last argument not starting with '-'
+            // TODO(bcall): process patttern!!!
+            path = @ptrCast(@constCast(arg));
         }
     }
 
     // replace forward slashes with backwards slashes for windows
-    for (0..dir_path.len) |i|
+    for (0..path.len) |i|
     {
-        if (dir_path[i] == '/') dir_path[i] = '\\';
+        if (path[i] == '/') path[i] = '\\';
     }
 
     //******************************
-    // get directory listings 
+    // get file data
     //******************************
-    const real_path = std.Io.Dir.cwd().realPathFileAlloc(io, dir_path, arena) catch {
-        stderr.print("ls: {s}: No such file or directory\n", .{dir_path}) catch {};
+    
+    const real_path = std.Io.Dir.cwd().realPathFileAlloc(io, path, arena) catch 
+    {
+        stderr.print("ls: {s}: No such file or directory\n", .{path}) catch {};
         stderr.flush() catch {};
         return;
     };
@@ -90,46 +94,18 @@ pub fn main(init: std.process.Init) !void
     // TODO(brendan): support other patterns like *.c, fileName.*, etc...
     const pattern = "*";
 
-    var dir_listings: ArrayList(DirEntry) = try .initCapacity(arena, 30);
-
-    const max_fname_width = try win32.getFileData(
-        arena, real_path, pattern, &dir_listings, include_all, recurse
+    var file_data: ArrayList(win32.FileData) = try .initCapacity(arena, 30);
+    const max_width = try win32.getFileData(
+        arena, real_path, pattern, show_hidden, recurse, &file_data
     );
 
     //******************************
-    // sort listings 
+    // sort files 
     //******************************
    
-    if (reverse)
-    {
-        if (sort_mod_time)
-        {
-            sort(DirEntry, dir_listings.items, {}, sortEntriesByModTimeReverse);
-        } 
-        else if (sort_size)
-        {
-            sort(DirEntry, dir_listings.items, {}, sortEntriesBySizeReverse);
-        }
-        else
-        {
-            sort(DirEntry, dir_listings.items, {}, sortEntriesByNameReverse);
-        }
-    }
-    else
-    {
-        if (sort_mod_time)
-        {
-            sort(DirEntry, dir_listings.items, {}, sortEntriesByModTime);
-        } 
-        else if (sort_size)
-        {
-            sort(DirEntry, dir_listings.items, {}, sortEntriesBySize);
-        }
-        else
-        {
-            sort(DirEntry, dir_listings.items, {}, sortEntriesByName);
-        }
-    }
+    if (sort_mtime)     { sort(win32.FileData, file_data.items, reverse, sortFilesByModTime); } 
+    else if (sort_size) { sort(win32.FileData, file_data.items, reverse, sortFilesBySize); }
+    else                { sort(win32.FileData, file_data.items, reverse, sortFilesByName); }
     
     const now = std.time.epoch.EpochSeconds{.secs = @intCast(std.Io.Timestamp.now(io, .real).toSeconds())};
     const year = now.getEpochDay().calculateYearDay().year;
@@ -144,19 +120,19 @@ pub fn main(init: std.process.Init) !void
     if (recurse)
     {
         // remove ending slash for display purposes
-        if (dir_path[dir_path.len-1] == '\\') dir_path.len -= 1;
+        if (path[path.len-1] == '\\') path.len -= 1;
 
         var current_idx: usize = 0;
-        while (current_idx < dir_listings.items.len)
+        while (current_idx < file_data.items.len)
         {
-            const listing: DirEntry = dir_listings.items[current_idx];
-            stdout.print("\n{s}{s}:\n", .{dir_path, listing.parent_dir_name[0..listing.parent_dir_name.len-1]}) catch {};
-            const next_idx = current_idx + listing.parent_dir_num_entries.*;
-            const subdir_listings: []DirEntry = dir_listings.items[current_idx..next_idx];
+            const file: win32.FileData = file_data.items[current_idx];
+            stdout.print("\n{s}{s}:\n", .{path, file.parent_name[0..file.parent_name.len-1]}) catch {};
+            const next_idx = current_idx + file.parent_num_entries.*;
+            const subfile_data: []win32.FileData = file_data.items[current_idx..next_idx];
             if (long_format) 
-                printLongFormat(stdout, subdir_listings, year)
+                printLongFormat(stdout, subfile_data, year)
             else 
-                printShortFormat(stdout, subdir_listings, listing.parent_dir_max_fname.*, false, row_wise);
+                printShortFormat(stdout, subfile_data, file.parent_max_width.*, false, row_wise); // TODO(bcall): before was max within parent dir...
             current_idx = next_idx;
         }
     }
@@ -165,14 +141,14 @@ pub fn main(init: std.process.Init) !void
     //******************
     else if (long_format)
     {
-        printLongFormat(stdout, dir_listings.items, year);
+        printLongFormat(stdout, file_data.items, year);
     }
     //******************
     // short format
     //******************
     else
     {
-        printShortFormat(stdout, dir_listings.items, max_fname_width, one_col, row_wise);
+        printShortFormat(stdout, file_data.items, max_width, one_col, row_wise);
     }
     stdout.flush() catch {};
 }
@@ -209,11 +185,11 @@ fn usage(writer: *Writer) void
 // output helper functions
 //******************************
 
-fn printShortFormat(writer: *Writer, dir_listings: []DirEntry, max_fname_width: usize, one_col: bool, row_wise: bool) void
+fn printShortFormat(writer: *Writer, file_data: []win32.FileData, max_width: usize, one_col: bool, row_wise: bool) void
 {
     const console_width: usize = win32.getConsoleWidth() orelse 80;
-    const nentries: usize = dir_listings.len;
-    const col_width: usize = (max_fname_width + 2); // 2 char padding b/w entries
+    const nentries: usize = file_data.len;
+    const col_width: usize = (max_width + 2); // 2 char padding b/w entries
     const ncol: usize = if (!one_col) @max(@divTrunc(console_width, col_width), 1) else 1;
     const nrow = std.math.divCeil(usize, nentries, ncol) catch unreachable;
 
@@ -224,15 +200,15 @@ fn printShortFormat(writer: *Writer, dir_listings: []DirEntry, max_fname_width: 
             const idx: usize = if (row_wise) row*ncol + col else row + col*(nrow);
             if (idx >= nentries) continue;
 
-            const listing = dir_listings[idx];
-            const color = if (listing.is_dir) BLUE else GREEN; 
+            const file = file_data[idx];
+            const color = if (file.is_dir) BLUE else GREEN; 
 
             // TODO(brendan) figure out how to print string with variable padding
-            writer.print("{s}{s}{s}", .{ color, listing.name, RESET }) catch {};
+            writer.print("{s}{s}{s}", .{ color, file.name, RESET }) catch {};
             // only add buffer between entries if multiple entries per line
             if (nrow != nentries)
             {
-                for (0..(col_width - listing.name.len)) |_|
+                for (0..(col_width - file.name.len)) |_|
                     writer.print(" ", .{}) catch {};
             }
         }
@@ -240,19 +216,20 @@ fn printShortFormat(writer: *Writer, dir_listings: []DirEntry, max_fname_width: 
     }
 }
 
-fn printLongFormat(writer: *Writer, dir_listings: []DirEntry, year: u16) void
+fn printLongFormat(writer: *Writer, file_data: []win32.FileData, year: u16) void
 {
-    for (dir_listings) |listing|
+    for (file_data) |file|
     {
-        const mod_time = listing.mod_time;
-        if (listing.is_dir)
+        const mod_time = win32.denseTimeToDateTime(file.last_write_time);
+
+        if (file.is_dir)
         {
             if (mod_time.year == year)
             {
                 writer.print("         {s} {d:>2} {:02}:{:02}  {s}{s}{s}\n", 
                     .{ 
                         MonthsStrTable[mod_time.month-1], mod_time.day, mod_time.hour, mod_time.minute,
-                        BLUE, listing.name, RESET 
+                        BLUE, file.name, RESET 
                 }) catch {};
             }
             else
@@ -260,20 +237,20 @@ fn printLongFormat(writer: *Writer, dir_listings: []DirEntry, year: u16) void
                 writer.print("         {s} {d:>2} {:>5}  {s}{s}{s}\n", 
                     .{ 
                         MonthsStrTable[mod_time.month-1], mod_time.day, mod_time.year,
-                        BLUE, listing.name, RESET 
+                        BLUE, file.name, RESET 
                 }) catch {};
             }
         }
         else 
         {
-            const file_size = formatSize(listing.size);
+            const file_size = formatSize(file.size);
             if (mod_time.year == year)
             {
                 writer.print("{:>6.1}{s}  {s} {d:>2} {:02}:{:02}  {s}{s}{s}\n", 
                     .{ 
                         file_size.size, UnitsStrTable[@intFromEnum(file_size.units)],
                         MonthsStrTable[mod_time.month-1], mod_time.day, mod_time.hour, mod_time.minute,
-                        GREEN, listing.name, RESET 
+                        GREEN, file.name, RESET 
                 }) catch {};
             }
             else
@@ -282,7 +259,7 @@ fn printLongFormat(writer: *Writer, dir_listings: []DirEntry, year: u16) void
                     .{ 
                         file_size.size, UnitsStrTable[@intFromEnum(file_size.units)],
                         MonthsStrTable[mod_time.month-1], mod_time.day, mod_time.year,
-                        GREEN, listing.name, RESET 
+                        GREEN, file.name, RESET 
                 }) catch {};
             }
         }
@@ -290,61 +267,37 @@ fn printLongFormat(writer: *Writer, dir_listings: []DirEntry, year: u16) void
 }
 
 //******************************
-// DirEntry sorting
+// File sorting
 //******************************
 
-fn sortEntriesByName(context: void, lhs: DirEntry, rhs: DirEntry) bool
+fn sortFilesByName(reverse: bool, lhs: win32.FileData, rhs: win32.FileData) bool
 {
-    _ = context;
-    const ord = order(u8, lhs.parent_dir_name, rhs.parent_dir_name);
+    const ord = order(u8, lhs.parent_name, rhs.parent_name);
     if (ord == .gt) return false;
     if (ord == .lt) return true;
-    return std.mem.lessThan(u8, lhs.name, rhs.name);
+
+    if (reverse) { return !std.mem.lessThan(u8, lhs.name, rhs.name); }
+    else         { return  std.mem.lessThan(u8, lhs.name, rhs.name); }
 }
 
-fn sortEntriesBySize(context: void, lhs: DirEntry, rhs: DirEntry) bool
+fn sortFilesBySize(reverse: bool, lhs: win32.FileData, rhs: win32.FileData) bool
 {
-    _ = context;
-    const ord = order(u8, lhs.parent_dir_name, rhs.parent_dir_name);
+    const ord = order(u8, lhs.parent_name, rhs.parent_name);
     if (ord == .gt) return false;
     if (ord == .lt) return true;
-    return lhs.size > rhs.size; // larger files first
+
+    if (reverse) { return lhs.size < rhs.size; }
+    else         { return lhs.size > rhs.size; } // larger files first by default 
 }
 
-fn sortEntriesByModTime(context: void, lhs: DirEntry, rhs: DirEntry) bool
+fn sortFilesByModTime(reverse: bool, lhs: win32.FileData, rhs: win32.FileData) bool
 {
-    _ = context;
-    const ord = order(u8, lhs.parent_dir_name, rhs.parent_dir_name);
+    const ord = order(u8, lhs.parent_name, rhs.parent_name);
     if (ord == .gt) return false;
     if (ord == .lt) return true;
-    return lhs.mod_time_dense > rhs.mod_time_dense; // newer files first
-}
 
-fn sortEntriesByNameReverse(context: void, lhs: DirEntry, rhs: DirEntry) bool
-{
-    _ = context;
-    const ord = order(u8, lhs.parent_dir_name, rhs.parent_dir_name);
-    if (ord == .gt) return false;
-    if (ord == .lt) return true;
-    return !std.mem.lessThan(u8, lhs.name, rhs.name);
-}
-
-fn sortEntriesBySizeReverse(context: void, lhs: DirEntry, rhs: DirEntry) bool
-{
-    _ = context;
-    const ord = order(u8, lhs.parent_dir_name, rhs.parent_dir_name);
-    if (ord == .gt) return false;
-    if (ord == .lt) return true;
-    return lhs.size < rhs.size; // smaller files first
-}
-
-fn sortEntriesByModTimeReverse(context: void, lhs: DirEntry, rhs: DirEntry) bool
-{
-    _ = context;
-    const ord = order(u8, lhs.parent_dir_name, rhs.parent_dir_name);
-    if (ord == .gt) return false;
-    if (ord == .lt) return true;
-    return lhs.mod_time_dense < rhs.mod_time_dense; // newer files first
+    if (reverse) { return lhs.last_write_time < rhs.last_write_time; }
+    else         { return lhs.last_write_time > rhs.last_write_time; } // newer files first by default
 }
 
 
@@ -356,7 +309,7 @@ const MonthsStrTable: [12][]const u8 = .{
 };
 
 //******************************
-// DirEntry size formatting
+// FileData size formatting
 //******************************
 const FileSize = struct 
 {
